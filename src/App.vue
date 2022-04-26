a07f4745f3d0760d2891a9aba94b9599a85dd262
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2022-04-26 12:21:57 +0530

    router integration to redirect the pages

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-  <h1 class="text-red-500 font-bold">Hello World</h1>
+ <router-view/>
 </template>
 
 <style>
