139d464abfcd90e7d8e2e89f36e92a70afc5d9a1
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2022-05-04 17:54:02 +0530

    some design changes in category list page and also add one extra date field in add category page

--- src/views/CategoryCreate.vue
+++ src/views/CategoryCreate.vue
@@ -2,9 +2,15 @@
 <div class="category-create">
  <div class="container mx-auto lg:w-1/2 w-full px-2">
  <h1 class="text-blue-500 my-8 font-bold text-2xl">Create New Category: </h1>
-        <div class="flex lg:flex-nowrap flex-wrap justify-center w-full">
-            <input type="text" id="text" class="bg-white rounded-md border border-gray-200 px-4 py-2 placeholder:text-slate-400 focus:outline-none w-full focus:ring-1 focus:ring-red-200" placeholder="Category Name" v-model="categoryName" >
-            <button class=" text-white px-4 py-2 font-bold rounded-lg ml-2 focus:outline-none focus:ring-1 focus:ring-violet-300 mt-4 lg:mt-0" :class="categoryName == ''?  'bg-red-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 cursor-pointer'"  @click="submit">SUBMIT</button>
+        <div class=" w-full">
+            <div class="mb-4">
+                <input type="text" id="text" class="bg-white lg:w-1/2 rounded-md border border-gray-200 px-4 py-2 placeholder:text-slate-400 focus:outline-none w-full focus:ring-1 focus:ring-red-200" placeholder="Category Name" v-model="categoryName" >
+            </div>
+            <div class="mb-4">
+                <input type="date" class="bg-white lg:w-1/2 rounded-md border border-gray-200 px-4 py-2 placeholder:text-slate-400 focus:outline-none w-full focus:ring-1 focus:ring-red-200" v-model="dueDate">
+            </div>
+                <button class=" text-white px-4 py-2 font-bold rounded-lg ml-2 focus:outline-none focus:ring-1 focus:ring-violet-300 mt-4 lg:mt-0" :class="categoryName == ''?  'bg-red-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 cursor-pointer'"  @click="submit">SUBMIT</button>
+            
        </div>
  </div>
 </div>
@@ -23,21 +29,23 @@ export default {
          const errMsg = ref(false)
          const router = useRouter()
          const toast = useToast();
+         const dueDate = ref('');
        
          const submit = () => {
              if(categoryName.value == ''){
                  toast.error("Please enter category name.");
-         
                 return;
             }
             const db = getFirestore();
             const docRef =  addDoc(collection(db, "categories"), {
             category : categoryName.value,
+            done: false, 
+            Date: dueDate.value 
         });  
             categoryName.value = ""
             router.push('/categories');
          } 
-         return{categoryName,submit, errMsg, toast};
+         return{categoryName,submit, errMsg, toast, dueDate};
     }
 }
 
--- src/views/categoriesList.vue
+++ src/views/categoriesList.vue
@@ -8,15 +8,28 @@
    <table class="border-collapse border border-slate-400 w-full text-black text-left">
         <thead class="text-center">
         <tr>
-            <th>SR NO.</th>
-            <th class="border border-slate-300 px-2 font-bold">Category Name</th>
+            <th class="p-2">SR NO.</th>
+            <th class="border border-slate-300 p-2 font-bold">Category Name</th>
+            <th class="border border-slate-300 p-2 font-bold">Status</th>
+            <th class="border border-slate-300 p-2 font-bold">Date</th>
+            <th class="border border-slate-300 p-2">Edit</th>
+            <th class="border border-slate-300 p-2">Delete</th>
         </tr>
         </thead>
         <tbody>
-            <tr v-for="(categories, categoryIndex) in categoriesList" :key="categoryIndex" class="text-center">
+            <tr v-for="(categories, categoryIndex) in categoriesList" :key="categoryIndex" class="text-center font-bold text-black">
                 <td class="border border-slate-300 px-2 py-2 ">{{ categoryIndex + 1 }}</td>
-                <td class="text-left px-2 py-2 capitalize border border-slate-300 ">
-                  {{categories.category}}</td>
+                <td class="text-left px-2 py-2 capitalize border border-slate-300 hover:text-blue-700">
+                <a href="#">{{categories.category}}</a>  
+                </td>
+                   <td class="border border-slate-300 px-2 py-2">
+                    <input type="checkbox" v-model="categories.done">
+                </td>
+                <td class="border border-slate-300 px-2 py-2">
+                    {{ categories.Date }}
+                </td>
+                <td class="border border-slate-300"> <span class="fa fa-pen"></span></td>
+                <td class="border border-slate-300"> <span class="fa fa-trash"></span></td>
             </tr>
         </tbody>
     </table>
@@ -39,8 +52,22 @@ export default{
         response.docs.forEach((doc) => {
           categoriesList.value.push(doc.data());
       });
+          console.log(categoriesList);
     })
       return{categoriesList,Router}
    }
   }
 </script>
+
+<style>
+   th {
+     background-color: rgb(76, 181, 238);
+     color: #ffffff;
+   }
+    tr:nth-child(even) {
+        background-color: rgb(251, 244, 244);
+    }
+    tr:nth-child(od) {
+        background-color: #fff;
+    }
+</style>
